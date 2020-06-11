4a85d0578beb52156d154a3410b482109d66d4ee
SEONMIN99 <seonmin99@naver.com>
2020-06-11 21:00:52 +0900

    Modify method and comment

--- RentalDVD.java
+++ RentalDVD.java
@@ -3,38 +3,28 @@
  * 랜탈 DVD 할인 요금을 계산하는 소프트웨어
  * 
  * @author (2018315034 정철우, 2018315038 이혜인, 2018315040 김선민) 
- * @version (2020)
+ * @version (2020.06.11)
  */
 public class RentalDVD
 {
     /**
      * 나이와 구작여부에 맞는 할인율을 반환하는 메소드
      * 
-     * @param age 사용자가 입력한 나이 
-     * @param oldVideo 구작 여부 / true: 구작. false: 신작
+     * @param age 나이 
+     * @param oldVideo 구작 여부 / true: 구작, false: 신작
      * @return discountRate 할인율  
      */
     public double calculate(int age, boolean oldVideo){
         double discountRate = 0;
-        if (age <= 18 && oldVideo == true){
+        if (oldVideo == true)
             discountRate = 0.5;
-        }
-        else if(age >= 65 && oldVideo == true){
-            discountRate = 0.5;
-        }
-        else{
-            if (age <= 18)
-            {
-                discountRate = 0.1;
-            }
-            else if(age >= 65)
-            {
-                discountRate = 0.2;
-            }
-            else if(oldVideo == true){
-                discountRate = 0.5;
-            }
-        }
+
+        else if (age <= 18)
+            discountRate = 0.1;
+
+        else if (age >= 65)
+            discountRate = 0.2;
+
         return discountRate;
     }
 }
